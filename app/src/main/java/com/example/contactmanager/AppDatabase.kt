37911cf5337f36f34dae9aa3bc23ea0550e36461
ChinmayGawad <chinmaygawad86@gmail.com>
2026-06-19 20:19:56 +0530

    Fix Duplicate Contacts

--- app/src/main/java/com/example/contactmanager/AppDatabase.kt
+++ app/src/main/java/com/example/contactmanager/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Contact::class], version = 1)
+@Database(entities = [Contact::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun contactDao(): ContactDao
 
@@ -19,7 +19,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "contact_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
--- app/src/main/java/com/example/contactmanager/Contact.kt
+++ app/src/main/java/com/example/contactmanager/Contact.kt
@@ -1,9 +1,12 @@
 package com.example.contactmanager
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "contacts")
+@Entity(tableName = "contacts",
+    indices = [Index(value = ["phoneNo"], unique = true)]
+)
 data class Contact(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
